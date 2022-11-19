c793fc30ed9b00474cbe6424655bd4322e3dfa35
ardhyy <mrardi131@gmail.com>
2022-11-19 23:15:49 +0800

    Update Controller API Transaksi

--- app/Http/Controllers/API/TransaksiController.php
+++ app/Http/Controllers/API/TransaksiController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use App\Models\Electre as kriteria;
 
 class TransaksiController extends Controller
 {
     public function transaksiStore(Request $request, $perusahaan)
+    {
+        $this->test($request, $perusahaan);
+        return response()->json([
+            'success' => true,
+            'message' => 'success',
+            'data' => $request->all()
+        ], 200);
+    }
+
+    private function test(Request $request, $perusahaan)
     {
         $validator = Validator::make($request->all(), [
             'jarak_tempuh' => 'required|string',
@@ -25,7 +36,7 @@ class TransaksiController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors());
         }
-        $biaya = "80000";
+        $biaya = "9000000";
         $total = $biaya * $request->jarak_tempuh;
 
         $data = [
@@ -34,7 +45,7 @@ class TransaksiController extends Controller
             'jumlah_muatan' => $request->jumlah_muatan,
             'total_biaya' => $total,
             'lama_sewa' => $hari,
-            'kriteria' => $request->kriteria
+            'kriteria' => $request->kriteria,
         ];
 
         $query = DB::table('tb_transaksi_detail_alternatif')->insert([
@@ -45,105 +56,53 @@ class TransaksiController extends Controller
             'lama_sewa' => $data['lama_sewa'],
         ]);
 
+        $dataKriteria = kriteria::get("id");
+        $res_kriteria = [
+            $dataKriteria[0]->id,
+            $dataKriteria[1]->id,
+            $dataKriteria[2]->id,
+            $dataKriteria[3]->id,
+        ];
 
         if ($data['jarak_tempuh'] < 250) {
-            $nilai_bobot = 5;
+            $bobot[] = 5;
         } else if ($data['jarak_tempuh'] <= 500) {
-            $nilai_bobot = 3;
+            $bobot[] = 3;
         } else if ($data['jarak_tempuh'] > 500) {
-            $nilai_bobot = 2;
+            $bobot[] = 2;
         }
 
         if ($data['jumlah_muatan'] < 600000) {
-            $nilai_bobot = 3;
+            $bobot[] = 3;
         } else if ($data['jumlah_muatan'] >= 1000000) {
-            $nilai_bobot = 4;
+            $bobot[] = 4;
         } else if ($data['jumlah_muatan'] > 1000000) {
-            $nilai_bobot = 5;
+            $bobot[] = 5;
         }
 
         if ($data['total_biaya'] < 200000000) {
-            $nilai_bobot = 2;
+            $bobot[] = 2;
         } else if ($data['total_biaya'] >= 500000000) {
-            $nilai_bobot = 3;
+            $bobot[] = 3;
         } else if ($data['total_biaya'] > 700000000) {
-            $nilai_bobot = 4;
+            $bobot[] = 4;
         }
 
         if ($data['lama_sewa'] < 25) {
-            $nilai_bobot = 4;
+            $bobot[] = 4;
         } else if ($data['lama_sewa'] <= 40) {
-            $nilai_bobot = 3;
+            $bobot[] = 3;
         } else if ($data['lama_sewa'] > 40) {
-            $nilai_bobot = 1;
+            $bobot[] = 1;
         }
-
-        $kritt = [
-            ["K01" => $data['jarak_tempuh']],
-            ["K02" => $data['jumlah_muatan']],
-            ["K03" => $data['total_biaya']],
-            ["K04" => $data['lama_sewa']],
-        ];
-
-        if ($data['kriteria'] == $data['jarak_tempuh']) {
-            $kriteria = $kritt[0];
-        } elseif ($data['kriteria'] == $data['jumlah_muatan']) {
-            $kriteria = $kritt[1];
-        } elseif ($data['kriteria'] == $data['total_biaya']) {
-            $kriteria = $kritt[2];
-        } elseif ($data['kriteria'] == $data['lama_sewa']) {
-            $kriteria = $kritt[3];
-        }
-
-        $dataAlternatif = [
-            [
-                'perusahaan' => $data['perusahaan'],
-                'kriteria' => $kritt[0],
-                'nilai' => $nilai_bobot
-            ],
-        ];
-        foreach ($dataAlternatif as $key => $value) {
-            $id[] = DB::table('tb_alternatif')->insertGetId(
+        foreach ($dataKriteria as $key => $value) {
+            $savedata[] = DB::table('tb_alternatif')->insertGetId(
                 [
-                    'perusahaan' => $value['perusahaan'],
-                    'kriteria' => $value['kriteria'],
-                    'nilai' => $value['nilai']
+                    'perusahaan' => $data['perusahaan'],
+                    'kriteria' => $res_kriteria[$key],
+                    'nilai' => $bobot[$key],
                 ]
             );
         }
-
-
-        echo json_encode($id[]);
-
-        // foreach ($kritt as $key => $no) {
-        //     $input['kriteria'] = $no;
-        //     $input['perusahaan '] = $dataAlternatif[$key];
-        //     $input['nilai'] = $dataAlternatif[$key];
-
-        //     $test = DB::table('tb_alternatif')->insert($input);
-        // }
-
-        return response()->json([
-            'success' => true,
-            'message' => 'success',
-            // 'data' => $test,
-        ], 200);
-    }
-
-    public function test(Request $request, $perusahaan)
-    {
-        $validator = Validator::make($request->all(), [
-            'jumlah_muatan' => 'required',
-        ]);
-        $data = [
-            'perusahaan' => $perusahaan,
-            'jumlah_muatan' => $request->jumlah_muatan = 1000,
-        ];
-
-        return response()->json([
-            'success' => true,
-            'message' => 'success',
-            'data' => $data,
-        ], 200);
     }
 }
